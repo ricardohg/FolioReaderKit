057ee94cf5c184bc371eaaf3ac8f60d557d28e48
Ricardo Hernandez <ricardohernandez@thepublicgroup.com>
2019-03-11 17:47:41 -0500

    moved toolbar to bottom

--- Source/ToolBarViewController.swift
+++ Source/ToolBarViewController.swift
@@ -15,9 +15,17 @@ class ToolBarViewController: UIViewController {
         case none
     }
     
+    enum EditDrawingType {
+        case undo
+        case redo
+    }
+    
     let pencilButton = UIButton(type: .custom)
+    let undoButton = UIButton(type: .custom)
+    let redoButton = UIButton(type: .custom)
     
     var toolSelected: ((Tool) -> ())?
+    var undoRedoAction: ((EditDrawingType) -> ())?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +40,25 @@ class ToolBarViewController: UIViewController {
         pencilButton.setImage(UIImage(readerImageNamed: "pencil"), for: .normal)
         pencilButton.setImage(UIImage(readerImageNamed: "pencil-selected"), for: .selected)
         
+        undoButton.setTitle("undo", for: .normal)
+        redoButton.setTitle("redo", for: .normal)
+        
         pencilButton.addTarget(self, action: #selector(pencilPressed), for: .touchUpInside)
+        undoButton.addTarget(self, action: #selector(undoPressed), for: .touchUpInside)
+        redoButton.addTarget(self, action: #selector(redoPressed), for: .touchUpInside)
         
         view.addSubview(pencilButton)
+//        view.addSubview(undoButton)
+//        view.addSubview(redoButton)
+        
         pencilButton.translatesAutoresizingMaskIntoConstraints = false
+        undoButton.translatesAutoresizingMaskIntoConstraints = false
+        redoButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+//        undoButton.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+//        undoButton.trailingAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         
         pencilButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         pencilButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
@@ -46,6 +69,14 @@ class ToolBarViewController: UIViewController {
     // MARK - Actions
     
     @objc func pencilPressed() {
-        self.toolSelected?(Tool.pencil)
+        self.toolSelected?(.pencil)
+    }
+
+    @objc func undoPressed() {
+        self.undoRedoAction?(.undo)
+    }
+    
+    @objc func redoPressed() {
+        self.undoRedoAction?(.undo)
     }
 }
